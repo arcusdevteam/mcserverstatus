77511ae3471ac212e19fe6c433131732b85f6998
arcus <jdtzmn@gmail.com>
2015-09-01 18:48:41 -0400

    Switched options to object

--- README.md
+++ README.md
@@ -3,25 +3,62 @@
 A javascript function for checking changes in a minecraft server's list of players without using query or a custom plugin.
 
 ## Usage
+
+### mcstatus(options,callback)
+
+Options can be an object:
+```js
+var options = {
+  name: 'name',
+  ip: 'ip'
+}
+```
+Or an array of objects:
+```js
+var options = [
+  {
+    name: 'name1',
+    ip: 'ip1'
+  },
+  {
+    name: 'name2',
+    ip: 'ip2'
+  }
+]
+```
+
+#### Example:
+
 ```js
 var mcstatus = require('mcserverstatus').mcstatus; // only include if using npm
 
-var ip = 'senious.mcph.co'; // the server ip
-
-var name = 'senious survival' // name can be anything
-
-mcstatus(ip, name, function response(response) {
+mcstatus({
+  name: 'senious survival', // can be anything
+  ip: 'senious.mcph.co' // the IP of the server
+}, function response(response) {
   console.log(response);
 });
 ```
-The response is in the form of a string and might look something like:
 
+If used in an interval, it will return the current players only once and then return consecutive join and leave messages.
+```js
+setInterval(function(){
+  mcstatus({
+    name: 'name',
+    ip: 'ip'
+  },function(response){
+    console.log(response);
+  });
+},1000);
+```
+#### Response:
+When first ran it returns:
 
-`fakeUsername joined fakeServerName. Now there are 5/50 players.`
-It will also respond if it finds that the server
-is offline, but it knows not to be annoying.
+`Currently [list of players] is playing on [server name].`
 
-If you run it once, it will remember that the server is offline, and not bug you again.
+Then if a player joins the server and the function is run again:
+
+`[playername] joined [server name]. Now there are [current players]/[max players] on the server, including [list of players].`
 
 ## Install
 
@@ -31,8 +68,4 @@ If you run it once, it will remember that the server is offline, and not bug you
 
 ### Without npm:
 
-`<script src="`[https://rawgit.com/arcusdevteam/mcserverstatus/master/mcstatus.js](https://rawgit.com/arcusdevteam/mcserverstatus/master/mcstatus.js)`"></script`
-
-## Demo
-
-Coming Soon...
+`<script src="`[https://rawgit.com/arcusdevteam/mcserverstatus/master/mcstatus.min.js](https://rawgit.com/arcusdevteam/mcserverstatus/master/mcstatus.min.js)`"></script`
--- mcstatus.js
+++ mcstatus.js
@@ -2,174 +2,206 @@ if (typeof exports !== 'undefined') {
   var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 }
 
-var mcstatus = function mcstatus(ip,name,callback) {
+var mcstatus = function mcstatus(options,callback) {
 
-  //creates a variable 'data' if not already defined:
-  if (typeof data === 'undefined') {
-    data = {};
+  // tests that the callback exists, it is a function, and that options is an object or array:
+  try {
+    if (!callback) throw new Error('Callback is required.');
+    if (typeof callback !== 'function') throw new Error('Callback must be a function');
+    if (typeof options !== 'object') throw new Error('Paramater options must be an object or an array.');
+  }
+  catch(err) {
+    callback(err);
   }
 
-  //defines other variables:
-  var playerlistarray = [], playerliststring = '';
-
-
-  // sends an XML request to mcping.net and sets global variable response to the xml.reponseText:
-  var xml = new XMLHttpRequest();
-  xml.onreadystatechange = function() {
-
-    // tests for a good response:
-    if (xml.readyState == 4 && xml.status == 200) {
-      response = JSON.parse(xml.responseText);
-
-      // tests for errors:
-      if (response.sample === null && response.online > 0 || response.max === 0) {
-        try {
-          if (response.sample === null && response.online > 0) throw 'Unfortunately, ' + name + ' is hiping their playerlist.';
-          if (response.max === 0) throw 'Could not find the ip for "' + name + '" or the server is not running.';
-        }
-        catch(err) {
-
-          // if there is no error data:
-          if (data === undefined || data !== undefined && data[ip + 'error'] === undefined && data[ip + 'error'] !== null) {
-            callback(err);
-            if (data === undefined) {
-              data = {};
-            }
-            data[ip + 'error'] = err;
-          }
-
-          // if there is error data:
-          else {
-
-            // if the error has already been reported:
-            if (data[ip + 'error'] !== err) {
-              callback(err);
-            }
-            data[ip + 'error'] = err;
-          }
-        }
-      }
-
-      // if there are no errors:
-      else {
-
-        // since there are no errors, setting error data to false:
-        if (data !== undefined && data[ip + 'error'] !== undefined && data[ip + 'error'] !== false) {
-          data[ip + 'error'] = false;
-        }
-
-        // if there are players online:
-        if (response.sample !== null && response.online < 13) {
-
-          // converts only the names of the players on the server into an array (playerlistarray):
-          for (i = 0; i < response.sample.length; i++) {
-            playerlistarray.push(response.sample[i].name);
-          }
-
-          // converts the playerlistarray variable into a string with correct grammar:
-          for (i = 0; i < playerlistarray.length; i++) {
-            if (playerlistarray.length == 1) {
-              playerliststring += playerlistarray[0];
-            }
-            else if (playerlistarray.length == 2) {
-              if (i+1 === playerlistarray.length) {
-                playerliststring += ' and ' + playerlistarray[1];
-              }
-              else {
-                playerliststring += playerlistarray[0];
-              }
-            }
-            else {
-              if (i+1 === playerlistarray.length) {
-                playerliststring += 'and ' + playerlistarray[i];
-              }
-              else {
-                playerliststring += playerlistarray[i] + ', ';
-              }
-            }
-          }
-        }
-
-        // if there are not players online:
-        else {
-          if (response.online > 12) {
-            callback('The server has too many players online to get an accurate list of players.');
-            return;
-          }
-        }
-
-        // checks for old data:
-        if (data === undefined || data !== undefined && data[ip] === undefined && data[ip] !== null) {
-
-          // callback if there are no players online and there is no data:
-          if (playerlistarray.length === 0) {
-            callback('Currently no players are playing on ' + name + '.');
-          }
-
-          // callback if there is one player online and there is no data:
-          else if (response.online === 1) {
-            callback('Currently ' + playerliststring + ' is playing on ' + name + '.');
-          }
-          else {
-            callback('Currently ' + playerliststring + ' are playing on ' + name + '.');
-          }
-
-          data[ip] = playerlistarray;
-        }
-
-        // if there is data:
-        else {
-
-          // if someone joined:
-          if (response.online - data[ip].length > 0) {
-
-            // if someone joined and one person is online:
-            if (response.online == 1) {
-              callback(capitalize(getChangeList(data[ip],playerlistarray,'join')) + ' joined ' + name + '. Now there are 1/' + response.max + '.');
-            }
-
-            // if someone joined and there is more than one person online:
-            else {
-              callback(capitalize(getChangeList(data[ip],playerlistarray,'join')) + ' joined ' + name + '. Now there are ' + response.online + '/' + response.max + ' players on the server, including ' + playerliststring + '.');
-            }
-          }
-
-          // if someone left:
-          if (response.online - data[ip].length < 0) {
-
-            if (playerlistarray === null) {
-              playerlistarray = [];
-            }
-
-            // if someone left and now there are no players online:
-            if (response.online === 0) {
-              callback(capitalize(getChangeList(data[ip],playerlistarray,'leave')) + ' left ' + name + '. Now there are 0/' + response.max + ' players on the server, so there are no players on the server.');
-            }
-
-            // if someone left and now there is only one player online:
-            else if (response.online == 1) {
-              callback(capitalize(getChangeList(data[ip],playerlistarray,'leave')) + ' left ' + name + '. Now there is 1/' + response.max + ' players on the server, and only ' + playerliststring + ' online.');
-            }
-
-            // if someone left and there is more than one person on the server:
-            else {
-              callback(capitalize(getChangeList(data[ip],playerlistarray,'leave')) + ' left ' + name + '. Now there are ' +  response.online + '/' + response.max + ' players on the server, including ' + playerliststring + '.');
-            }
-          }
-
-          // stores data:
-          data[ip] = playerlistarray;
-        }
-      }
+    // turns options into an array with one object in preperation for the loop:
+    if (Array.isArray(options) !== true) {
+      options = [options];
     }
-  };
-  var url = 'http://mcping.net/api/' + ip + '/online,max,sample,strippedmotd';
-  xml.open('GET', url, 'true');
-  xml.send();
 
+  var xml = [];
+
+  // for loop to run through the array:
+  for (var i in options) {
+
+    // function to fix XMLHttpRequest variable scope
+    (function(i) {
+
+      // creates a variable 'data' if not already defined:
+      if (typeof data === 'undefined') {
+        data = {};
+      }
+
+      // verifies that ip is defined:
+      try {
+        if (options[i].ip === undefined) throw new Error('Ip is required for the server ' + options[i].name + '.');
+      }
+      catch(err) {
+            callback(err);
+      }
+
+      // otherwise sets the variables:
+      var ip = options[i].ip;
+      var name = options[i].name || ip; //name defaults to the ip
+
+      // defines other variables:
+      var playerlistarray = [], playerliststring = '';
+
+
+      // sends an XML request to mcping.net and sets global variable response to the xml.reponseText:
+      xml[i] = new XMLHttpRequest();
+      xml[i].onreadystatechange = function() {
+
+        // tests for a good response:
+        if (xml[i].readyState == 4 && xml[i].status == 200) {
+          response = JSON.parse(xml[i].responseText);
+
+          // tests if the server is hiding their playerlist, or if the server is not running:
+          if (response.sample === null && response.online > 0 || response.max === 0) {
+            try {
+              if (response.sample === null && response.online > 0) throw 'Unfortunately, ' + name + ' is hiding their playerlist.';
+              if (response.max === 0) throw 'Could not find the ip for "' + name + '" or the server is not running.';
+            }
+            catch(err) {
+
+              // if there is no error data:
+              if (data[ip + 'error'] === undefined && data[ip + 'error'] !== null) {
+                callback(err);
+                data[ip + 'error'] = err;
+              }
+
+              // if there is error data:
+              else {
+
+                // if the error has already been reported:
+                if (data[ip + 'error'] !== err) {
+                  callback(err);
+                }
+                data[ip + 'error'] = err;
+              }
+            }
+          }
+
+          // if there are no errors:
+          else {
+
+            // since there are no errors, setting error data to false:
+            if (data !== undefined && data[ip + 'error'] !== undefined && data[ip + 'error'] !== false) {
+              data[ip + 'error'] = false;
+            }
+
+            // if there are players online:
+            if (response.sample !== null && response.online < 13) {
+
+              // converts only the names of the players on the server into an array (playerlistarray):
+              for (i = 0; i < response.sample.length; i++) {
+                playerlistarray.push(response.sample[i].name);
+              }
+
+              // converts the playerlistarray variable into a string with correct grammar:
+              for (i = 0; i < playerlistarray.length; i++) {
+                if (playerlistarray.length == 1) {
+                  playerliststring += playerlistarray[0];
+                }
+                else if (playerlistarray.length == 2) {
+                  if (i+1 === playerlistarray.length) {
+                    playerliststring += ' and ' + playerlistarray[1];
+                  }
+                  else {
+                    playerliststring += playerlistarray[0];
+                  }
+                }
+                else {
+                  if (i+1 === playerlistarray.length) {
+                    playerliststring += 'and ' + playerlistarray[i];
+                  }
+                  else {
+                    playerliststring += playerlistarray[i] + ', ';
+                  }
+                }
+              }
+            }
+
+            // if there are not players online:
+            else {
+              if (response.online > 12) {
+                callback('The server "' + name + '" has too many players online to get an accurate list of players.');
+                return;
+              }
+            }
+
+            // checks for old data:
+            if (data === undefined || data !== undefined && data[ip] === undefined && data[ip] !== null) {
+
+              // callback if there are no players online and there is no data:
+              if (playerlistarray.length === 0) {
+                callback('Currently no players are playing on ' + name + '.');
+              }
+
+              // callback if there is one player online and there is no data:
+              else if (response.online === 1) {
+                callback('Currently ' + playerliststring + ' is playing on ' + name + '.');
+              }
+              else {
+                callback('Currently ' + playerliststring + ' are playing on ' + name + '.');
+              }
+
+              data[ip] = playerlistarray;
+            }
+
+            // if there is data:
+            else {
+
+              // if someone joined:
+              if (response.online - data[ip].length > 0) {
+
+                // if someone joined and one person is online:
+                if (response.online == 1) {
+                  callback(capitalize(getChangeList(data[ip],playerlistarray,'join')) + ' joined ' + name + '. Now there are 1/' + response.max + '.');
+                }
+
+                // if someone joined and there is more than one person online:
+                else {
+                  callback(capitalize(getChangeList(data[ip],playerlistarray,'join')) + ' joined ' + name + '. Now there are ' + response.online + '/' + response.max + ' players on the server, including ' + playerliststring + '.');
+                }
+              }
+
+              // if someone left:
+              if (response.online - data[ip].length < 0) {
+
+                if (playerlistarray === null) {
+                  playerlistarray = [];
+                }
+
+                // if someone left and now there are no players online:
+                if (response.online === 0) {
+                  callback(capitalize(getChangeList(data[ip],playerlistarray,'leave')) + ' left ' + name + '. Now there are 0/' + response.max + ' players on the server, so there are no players on the server.');
+                }
+
+                // if someone left and now there is only one player online:
+                else if (response.online == 1) {
+                  callback(capitalize(getChangeList(data[ip],playerlistarray,'leave')) + ' left ' + name + '. Now there is 1/' + response.max + ' players on the server, and only ' + playerliststring + ' online.');
+                }
+
+                // if someone left and there is more than one person on the server:
+                else {
+                  callback(capitalize(getChangeList(data[ip],playerlistarray,'leave')) + ' left ' + name + '. Now there are ' +  response.online + '/' + response.max + ' players on the server, including ' + playerliststring + '.');
+                }
+              }
+
+              // stores data:
+              data[ip] = playerlistarray;
+            }
+          }
+        }
+      };
+      var url = 'http://mcping.net/api/' + ip + '/online,max,sample,strippedmotd';
+      xml[i].open('GET', url, 'true');
+      xml[i].send();
+    })(i);
+  }
 
   // functions:
-
   function getChangeList(oldList,newList,method) {
 
     // function to get the differences between two arrays:
@@ -220,7 +252,7 @@ var mcstatus = function mcstatus(ip,name,callback) {
     return changeliststring;
   }
 
-  //function to make the first letter capitalized:
+  // function to make the first letter capitalized:
   function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
@@ -228,6 +260,10 @@ var mcstatus = function mcstatus(ip,name,callback) {
 if (typeof exports !== 'undefined') {
   exports.mcstatus = mcstatus;
 }
-mcstatus('198.24.164.162:34009','koth',function(response){
-  console.log(response);
-});
+setInterval(function(){
+  mcstatus({
+    ip: 'enderman.biz'
+  },function(response){
+    console.log(response);
+  });
+},1000);
