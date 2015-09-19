b8cf8fa2d00ba789f5303e9646de84f535102ad7
sdaitzman <samuel@daitzman.com>
2015-09-19 14:32:55 -0400

    initial gulpfile commit

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp   = require('gulp');
+var watch  = require('gulp-watch');
+var uglify = require('gulp-uglify');
+
+gulp.task('js', function() {
+    return gulp.src('./mcstatus.js')
+        .pipe(watch('./mcstatus.js'))
+        .pipe(gulp.dest('./mcstatus.min.js'))
+});
